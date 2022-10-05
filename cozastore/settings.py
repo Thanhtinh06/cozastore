690fcc70be173509c4310a6a95ef87a2b9197c96
Thanhtinh06 <nguyenthithanhtinh06@gmail.com>
2022-10-05 22:59:51 +0700

    add CSRF_FAILURE_VIEW

--- cozastore/settings.py
+++ cozastore/settings.py
@@ -163,3 +163,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+
+CSRF_FAILURE_VIEW = 'cozastore.views.csrf_failure'
